bfff9e92c51d4e2b6c5a693ab414a29b97c849bf
Roman-orn <0671635566@ukr.net>
2026-05-03 21:37:08 +0300

    Added check for empty array and null value

--- src/DataHandler.java
+++ src/DataHandler.java
@@ -1,9 +1,12 @@
 public class DataHandler {
 
-    public <T> String handleData(T[] array){
+    public <T> String handleData(T[] array) {
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException("No data available!");
+        }
         StringBuilder sb = new StringBuilder();
         int count = 0;
-        for (T item : array){
+        for (T item : array) {
             count++;
             sb.append(String.format("(%d) %s; ", count, item));
         }
