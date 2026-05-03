5f45ad05b465a2648bf27e13ec89af3b361b0761
Roman-orn <0671635566@ukr.net>
2026-05-03 12:29:43 +0300

    Added functionality to the Main class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,19 @@
 public class Main {
 
+    public static void main(String[] args){
+
+        DataProvider provider = new DataProvider();
+        DataHandler dataHandler = new DataHandler();
+
+        String productsName = dataHandler.handleData(provider.getProductNames());
+        getOutPut("Products: " + productsName);
+
+        String salesAmount = dataHandler.handleData(provider.getSalesAmounts());
+        getOutPut("Sales, EUR: " + salesAmount);
+    }
+
+    private static void getOutPut(String output){
+        System.out.println(output);
+    }
+
 }
